069e740ee18ca073b8b1e133e639df328d1bf957
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-01-16 12:00:12 +0600

    Added data file for the travel data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import bandarban from "./images/Bandarban.jpg";
+import sylhet from "./images/sylhet.jpg";
+export default [{
+        title: "Bandarban",
+        location: "Bangladesh",
+        googleMapsUrl: "https://www.google.com/maps/place/Bandarban,+Bangladesh/@22.206713,92.1977376,14z/data=!3m1!4b1!4m5!3m4!1s0x30ad6a188414e62b:0xa4c3198f81ba90d0!8m2!3d22.1935628!4d92.2187476",
+        startDate: "12 Jan, 2017",
+        endDate: "16 Jan, 2017",
+        description: "Bandarban Hill District is blissed with beauties & culture. You’ll find great mountain views in bandarban. It treks through virgin forests and chance to meet more than 15 tribes. Bandarban town which lies on the Sangu River, 92km from Chittagong. The river is the center of local life: epically long bamboo rafts, steered by a solitary boatman.",
+        imageUrl: bandarban,
+    },
+    {
+        title: "Sylhet",
+        location: "Bangladesh",
+        googleMapsUrl: "https://www.google.com/maps/place/Sylhet,+Bangladesh/@24.8998373,91.8259623,13z/data=!3m1!4b1!4m5!3m4!1s0x375054d3d270329f:0xf58ef93431f67382!8m2!3d24.8949294!4d91.8687063",
+        startDate: "24 Dec, 2018",
+        endDate: "28 Jan, 2018",
+        description: "Sylhet is a city in eastern Bangladesh, on the Surma River. It’s known for its Sufi shrines, like the ornate tomb and mosque of 14th-century saint Hazrat Shah Jalal, now a major pilgrimage site near Dargah Gate. The tiny Museum of Rajas contains belongings of the local folk poet Hasan Raja. ",
+        imageUrl: sylhet,
+    },
+    {
+        title: "Cox Bazar",
+        location: "Bangladesh",
+        googleMapsUrl: "https://www.google.com/maps/place/Cox's+Bazar,+Bangladesh/@21.4510434,91.9328612,12z/data=!3m1!4b1!4m5!3m4!1s0x30adc7ea2ab928c3:0x3b539e0a68970810!8m2!3d21.4272283!4d92.0058074",
+        startDate: "2 Feb, 2012",
+        endDate: "4 Feb, 2012",
+        description: "Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts.",
+        imageUrl: cox,
+    },
+];
