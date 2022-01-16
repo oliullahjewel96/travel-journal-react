ebd07e11bebe5c6d9a55a312a563304833685838
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-01-16 12:48:00 +0600

    Styled the navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css";
+export default function Nav() {
+  return (
+    <div>
+      <nav>
+        <p className="nav--header">My Travel Journal</p>
+      </nav>
+    </div>
+  );
+}
